f822017670aeaba7865523954828082b9302b6a1
leopold1511 <bavrinleonid@gmail.com>
2024-04-14 22:45:42 +0300

    Create ExcelParser

--- src/main/java/Lab2/ExcelParser.java
+++ src/main/java/Lab2/ExcelParser.java
@@ -8,7 +8,6 @@ import java.util.Map;
 import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 
